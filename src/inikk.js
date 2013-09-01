a6caf2ec289feb63e7c9eff8d1d1992881259ee8
Lauri Kaitala <lawkaita@gmail.com>
2013-09-01 19:58:07 +0300

    support adding monsters

--- src/inikk.js
+++ src/inikk.js
@@ -3,8 +3,14 @@ var database = [
     {name: 'kobold123', inikka: 36, hp: 77}
 ]
 
+function addMonster(name, initiative, hp) {
+    var monster = {name: name, inikka: initiative, hp: hp};
+    database.push(monster);
+}
+
 function init() {
     console.log(11345);
+    addMonster('Aboleth', 12, 100);
     var foo = document.getElementById('main');
     var text = createUi(database);
     foo.appendChild(text);
@@ -20,6 +26,35 @@ function createRow(creature) {
     return row;
 }
 
+function addClicked() {
+    var nameElement = document.getElementById('name');
+    var name = nameElement.value;
+    addMonster(name, 0, 0);
+
+    console.log('pekka' + name);
+}
+
+function createAddUi() {
+    var nameInput = document.createElement('input');
+    nameInput.setAttribute('type', 'text');
+    nameInput.setAttribute('id', 'name');
+    var nameCell = document.createElement('td');
+    nameCell.appendChild(nameInput);
+
+    var button = document.createElement('input');
+    button.setAttribute('type', 'button');
+    button.addEventListener('click', addClicked);
+
+    var buttonCell = document.createElement('td');
+    buttonCell.appendChild(button);
+    
+    var row = document.createElement('tr');
+    row.appendChild(nameCell);
+    row.appendChild(buttonCell);
+
+    return row;
+}
+
 function createUi(creatures) {
     var table = document.createElement('table');
     table.setAttribute('id', 'ui');
@@ -28,6 +63,8 @@ function createUi(creatures) {
         var testRow = createRow(creature);
         table.appendChild(testRow);
     }
+    var addUi = createAddUi();
+    table.appendChild(addUi);
     return table;    
 }
 
